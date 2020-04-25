7b47036a24dba64f47a4acb86203e7ca9ca36ec5
djuka6 <djukicdjordje98@gmail.com>
2020-04-25 21:58:54 +0200

    Updates

---
+++ precache-manifest.df94f79831227aa704fbade234df1b89.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "138e9b05f04525762733d8ccc1ed4b9f",
+    "url": "/oglasi-app/index.html"
+  },
+  {
+    "revision": "a85e8161949069474474",
+    "url": "/oglasi-app/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "457f0119af99cdae9844",
+    "url": "/oglasi-app/static/css/main.c80ad70b.chunk.css"
+  },
+  {
+    "revision": "a85e8161949069474474",
+    "url": "/oglasi-app/static/js/2.381d357d.chunk.js"
+  },
+  {
+    "revision": "d09ac505fec0e9130a1d5b85fb3fa6dc",
+    "url": "/oglasi-app/static/js/2.381d357d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "457f0119af99cdae9844",
+    "url": "/oglasi-app/static/js/main.4b946ad4.chunk.js"
+  },
+  {
+    "revision": "cc1d90b998edfc5e8a0e",
+    "url": "/oglasi-app/static/js/runtime-main.7b2a8617.js"
+  }
+]);
